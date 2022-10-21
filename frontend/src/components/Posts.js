0e311d0a9acab26e9ad82f5cf058a77b1a067fa2
SelenaHan <shuo.han@edu.sait.ca>
2022-10-21 12:08:22 -0600

    new test

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -7,6 +7,7 @@ const Posts = ({contents}) => {
         {contents.map((post, index) => (
             <div>
             <PostCard title={post.title} post_content={post.post_content} id={post._id} key={index}/>
+            <h1>Hellos</h1>
             </div>
         ))}
     </div>

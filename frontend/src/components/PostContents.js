66e7eb06fbee80377889476fd3889863af14bda1
SelenaHan <shuo.han@edu.sait.ca>
2022-10-22 17:18:29 -0600

    new update

--- frontend/src/components/PostContents.js
+++ frontend/src/components/PostContents.js
@@ -3,30 +3,133 @@
 
 const PostContents = [
     {
-        title: "test",
-        post_content: ["- This is ", <strong>not</strong>,  " working.", <br/>, "- This is ", <strong>not</strong>,  " working.",],
-        post_img: "https://raw.githubusercontent.com/SelenaHan/myblog/main/frontend/src/Images/code.jpg",
+        order: "1",
+        title: [<h2>Introduction</h2>,],
+        post_content: [
+            "-	Welcome to my blog, in this blog I will talk about how to use NodeJS(for Back-end), MongoDB(for Database), React(for Front-end) to build a blog post website like this.", <br/>,
+            "-	We can use this website post new posts and edit or delete new post, I also want to add a leave a like or comments or subscribe functions, I will keep updating",  <br/>,
+            "-	I use Bootstrap to style my website so that my website can be displayed very nicely on screens of different sizes", <br/>,
+            "-	I also write down about how to deploy a Ract project on GitHub, so that other people can see this blog", <br/>,
+            "-	This is the GitHub link of the project, you are welcome to check out the code of the project", <br/>,
+            "-	Hope you could gwt something from this post, I would aapreciate it if you have good advice or seggestion or anything please contact me. Happy coding, Enjoy!", <br/>,
+            "-	Happy coding, Enjoy!", <br/>,
+        ],
+        post_img: "https://raw.githubusercontent.com/SelenaHan/myblog/main/frontend/src/Images/1-1.jpg",
     },
     {
-        title: "test1",
+        order: "1-1",
+        title: [<h2>I. Setup</h2>,<br/>,"1. Software this project will use: "],
+        post_content: [
+            "-	NodeJS: this is for our back end.", <br/>,
+            "-	VS Code: for editors, a lot of React developers recommended VS Code, but other editors work too.",  <br/>,
+            "-	Postman: this app will help us to test out whether a connection was successful or not and also our functions works or not.", <br/>,
+        ],
+        post_img: "https://raw.githubusercontent.com/SelenaHan/myblog/main/frontend/src/Images/1-1.jpg",
+    },
+    {
+        order: "1-2",
+        title: "2.	Tools",
         post_content: "- content<br/>- content<br/>- content<br/>",
-        post_img:  "https://help.apple.com/assets/6226A2B733970465D6250C17/6226A2B933970465D6250C41/en_GB/506a2faf4b87ede7a358bed134342968.png",
+        post_img:  "https://raw.githubusercontent.com/SelenaHan/myblog/main/frontend/src/Images/1-2.jpg",
     },
     {
+        order: "1-3",
         title: "test2",
         post_content: "- content<br/>- content<br/>- content<br/>",
-        post_img:  "https://github.com/SelenaHan/myblog/blob/main/frontend/src/Images/code.jpg",
+        post_img:  "https://raw.githubusercontent.com/SelenaHan/myblog/main/frontend/src/Images/1-3.jpg",
     },
     {
+        order: "1-4",
         title: "test3",
         post_content: "- content<br/>- content<br/>- content<br/>",
-        post_img:  "https://github.com/SelenaHan/myblog/blob/main/frontend/src/Images/code.jpg",
+        post_img:  "https://raw.githubusercontent.com/SelenaHan/myblog/main/frontend/src/Images/1-4.jpg",
     },
     {
+        order: "1-5",
+        title: "test4",
+        post_content: "- content<br/>- content<br/>- content<br/>",
+        post_img: "https://raw.githubusercontent.com/SelenaHan/myblog/main/frontend/src/Images/1-5.jpg",
+    },
+    {
+        order: "1-5",
         title: "test4",
         post_content: "- content<br/>- content<br/>- content<br/>",
         post_img: "https://github.com/SelenaHan/myblog/blob/main/frontend/src/Images/code.jpg",
     },
+    {
+        order: "1-5",
+        title: "test4",
+        post_content: "- content<br/>- content<br/>- content<br/>",
+        post_img: "https://github.com/SelenaHan/myblog/blob/main/frontend/src/Images/code.jpg",
+    },
+    {
+        order: "1-5",
+        title: "test4",
+        post_content: "- content<br/>- content<br/>- content<br/>",
+        post_img: "https://github.com/SelenaHan/myblog/blob/main/frontend/src/Images/code.jpg",
+    },
+    {
+        order: "1-5",
+        title: "test4",
+        post_content: "- content<br/>- content<br/>- content<br/>",
+        post_img: "https://github.com/SelenaHan/myblog/blob/main/frontend/src/Images/code.jpg",
+    },
+
+    {
+        order: "1-5",
+        title: "test4",
+        post_content: "- content<br/>- content<br/>- content<br/>",
+        post_img: "https://github.com/SelenaHan/myblog/blob/main/frontend/src/Images/code.jpg",
+    },
+    {
+        order: "1-5",
+        title: "test4",
+        post_content: "- content<br/>- content<br/>- content<br/>",
+        post_img: "https://github.com/SelenaHan/myblog/blob/main/frontend/src/Images/code.jpg",
+    },
+    {
+        order: "1-5",
+        title: "test4",
+        post_content: "- content<br/>- content<br/>- content<br/>",
+        post_img: "https://github.com/SelenaHan/myblog/blob/main/frontend/src/Images/code.jpg",
+    },
+    {
+        order: "1-5",
+        title: "test4",
+        post_content: "- content<br/>- content<br/>- content<br/>",
+        post_img: "https://github.com/SelenaHan/myblog/blob/main/frontend/src/Images/code.jpg",
+    },
+    {
+        order: "1-5",
+        title: "test4",
+        post_content: "- content<br/>- content<br/>- content<br/>",
+        post_img: "https://github.com/SelenaHan/myblog/blob/main/frontend/src/Images/code.jpg",
+    },
+    {
+        order: "1-5",
+        title: "test4",
+        post_content: "- content<br/>- content<br/>- content<br/>",
+        post_img: "https://github.com/SelenaHan/myblog/blob/main/frontend/src/Images/code.jpg",
+    },
+    {
+        order: "1-5",
+        title: "test4",
+        post_content: "- content<br/>- content<br/>- content<br/>",
+        post_img: "https://github.com/SelenaHan/myblog/blob/main/frontend/src/Images/code.jpg",
+    },
+    {
+        order: "1-5",
+        title: "test4",
+        post_content: "- content<br/>- content<br/>- content<br/>",
+        post_img: "https://github.com/SelenaHan/myblog/blob/main/frontend/src/Images/code.jpg",
+    },
+    {
+        order: "1-5",
+        title: "test4",
+        post_content: "- content<br/>- content<br/>- content<br/>",
+        post_img: "https://github.com/SelenaHan/myblog/blob/main/frontend/src/Images/code.jpg",
+    },
+
 
 ]
 
